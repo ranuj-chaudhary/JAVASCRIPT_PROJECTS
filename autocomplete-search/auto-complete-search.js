cc1f799e636e374f4b7455974ff573cc008fefcc
RANUJ CHAUDHARY <ranujchoudhary@gmail.com>
2024-03-24 19:40:51 +0530

    auto-complete search

--- autocomplete-search/auto-complete-search.js
+++ autocomplete-search/auto-complete-search.js
@@ -1,6 +1,8 @@
+"use strict";
+
 const searchBtn = document.querySelector(".search-btn");
 const inputEle = document.getElementById("input-box");
-
+const resultBox = document.querySelector(".result-box ul");
 async function fetchProducts() {
   try {
     const res = await fetch(`https://dummyjson.com/products?limit=100`);
@@ -10,50 +12,57 @@ async function fetchProducts() {
     }
 
     const data = await res.json();
-
+console.log(data.products);
     // input
-    if (data.products.length > 0 && data.products) {
+    if (data.products.length > 0 && data.products.length === 100) {
       // get search results for query
       return data.products;
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
 
 function SearchResults(products, query) {
+  // get all title from products fetched
   const titles = products.map((product) => product.title);
   if (query.length > 0) {
+    // get search result as per input by user
     const results = titles
       .map((title) => title.toLowerCase())
       .filter((title) => title.includes(query));
     return results;
   } else if (query.length === 0) {
-    alert("Input is empty. Enter query to get results.");
+    resultBox.innerHTML = "";
     return false;
   }
 }
 
 function displaySearchResults(results) {
   if (results.length > 0 && results) {
-    const searchList = results.map(
-      (result) =>
-        `<li onclick="insertSearchResultToInputElement(this)">${result}</li>`
-    );
-    const resultBox = document.querySelector(".result-box ul");
+    const searchList = results.map((result) => `<li>${result}</li>`);
     resultBox.innerHTML = searchList.join("");
   } else if (results.length === 0 && results) {
-    console.error("No results fetched");
+    resultBox.innerHTML = "<li>No result found</li>";
   }
 }
 
-function insertSearchResultToInputElement(ele) {
+function insertListToInput(ele) {
   inputEle.value = ele.innerText;
   // clear list after selection of search suggestions
-  const resultBox = document.querySelector(".result-box ul");
   resultBox.innerHTML = "";
 }
 
+// EVENT HANDLERS
+
+// Insert list innertext to input field
+
+resultBox.addEventListener("click", (e) => {
+  inputEle.value = e.target.innerText;
+  resultBox.innerHTML = "";
+});
+
+// Search box keypress event handler
 inputEle.addEventListener("keypress", (e) => {
   (async () => {
     // fetch products from api
@@ -67,27 +76,29 @@ inputEle.addEventListener("keypress", (e) => {
     // display search results on screen
     displaySearchResults(searchResult);
   })();
-
-  // if (e.key === "Enter") {
-  //   e.preventDefault();
-
-  //   (async () => {
-  //     // fetch products from api
-  //     const fetchedProducts = await fetchProducts();
-
-  //     const query = inputEle.value;
-
-  //     // get search result for query
-  //     const searchResult = SearchResults(fetchedProducts, query);
-
-  //     // display search results on screen
-  //     displaySearchResults(searchResult);
-  //   })();
-
-  // }
 });
 
-searchBtn.addEventListener("click", (e) => {
+inputEle.addEventListener("keyup", (e) => {
+  if (e.key === "Backspace") {
+    (async () => {
+      // fetch products from api
+      const fetchedProducts = await fetchProducts();
+
+      const query = inputEle.value;
+
+      // get search result for query
+      const searchResult = SearchResults(fetchedProducts, query);
+
+      // display search results on screen
+      displaySearchResults(searchResult);
+    })();
+  } else {
+    return;
+  }
+});
+
+// Search box search icon click event handler
+searchBtn.addEventListener("click", () => {
   const icon = document.querySelector(".fa-search");
   const closest = icon.closest(".search-btn");
 
@@ -104,3 +115,10 @@ searchBtn.addEventListener("click", (e) => {
     })();
   }
 });
+
+export {
+  fetchProducts,
+  SearchResults,
+  displaySearchResults,
+  insertListToInput,
+};
---
+++ autocomplete-search/auto-complete-search.test.js
@@ -0,0 +1,8 @@
+import {
+  fetchProducts,
+  SearchResults,
+  displaySearchResults,
+  insertListToInput,
+} from "./auto-complete-search.js";
+
+console.log(fetchProducts);
